55d45848f7f372872f1a5d6c3d392d0fcc94b0f8
Silas Reinagel <sreinagel@Silass-MacBook-Air.local>
2025-10-03 20:45:52 -0500

    Coding: Scriptable Object - Exhibit. Closes #25

---
+++ src/LDJam58/Assets/Scripts/Game/Exhibits/ExhibitTag.cs
@@ -0,0 +1,19 @@
+    public enum ExhibitTag
+    {
+        None = 0,
+        TimePeriod_Prehistoric = 1,
+        TimePeriod_Ancient = 2,
+        TimePeriod_Medieval = 3,
+        TimePeriod_EarlyModern = 4,
+        TimePeriod_Modern = 5,
+
+        ObjectType_Sculpture = 500,
+        ObjectType_Bones = 501,
+        ObjectType_Warfare = 502,
+        ObjectType_Treasure = 503,
+        ObjectType_InventionsAndInstruments = 504,
+
+        Genre_Gothic = 1000,
+        Genre_Pirate = 1001,
+        Genre_ReligiousOrRitual = 1002
+    }
